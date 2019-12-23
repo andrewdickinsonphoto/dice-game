767c49dce10684748875d887a67170622e8fcd04
andrewdickinsonphoto <58611502+andrewdickinsonphoto@users.noreply.github.com>
2019-12-23 18:17:54 -0500

    Added button to play vs. refreshing page to play.

--- index.js
+++ index.js
@@ -1,19 +1,47 @@
+document.querySelector(".myButton").addEventListener("click", function() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+  if (randomNumber1 == randomNumber2) {
+    document.querySelector("h1").textContent = "Draw!";
+  } else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").textContent = "Player 1 wins!";
+  } else {
+    document.querySelector("h1").textContent = "Player 2 wins!";
+  }
+});
+
 // Random Number Generation
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+// var randomNumber1 = 6;
+// var randomNumber2 = 6;
+//
+// function selectRandomNumbers() {
+//
+//   var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+//   var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+//
+// }
 
 // Image Replacement to Match Random Number
 
-document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+// function changeImages() {
+//
+//   document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+//   document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+//
+// }
 
 // If Statement To Determine Winner Text
 
-if (randomNumber1 == randomNumber2) {
-  document.querySelector("h1").textContent = "Draw!";
-} else if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").textContent = "Player 1 wins!";
-} else {
-  document.querySelector("h1").textContent = "Player 2 wins!";
-}
+// function declareWinner() {
+//   if (randomNumber1 == randomNumber2) {
+//     document.querySelector("h1").textContent = "Draw!";
+//   } else if (randomNumber1 > randomNumber2) {
+//     document.querySelector("h1").textContent = "Player 1 wins!";
+//   } else {
+//     document.querySelector("h1").textContent = "Player 2 wins!";
+//   }
+//
+// }
